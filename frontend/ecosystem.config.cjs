38e93abf7acd5c85d425ee10067f3a13e2885538
Luiz Gustavo <luizguestape54@hotmail.com>
2025-03-14 16:39:55 -0300

    Update ecosystem.config.cjs

--- frontend/ecosystem.config.cjs
+++ frontend/ecosystem.config.cjs
@@ -1,23 +1,24 @@
 module.exports = {
-  apps: [{
-    name: 'planejador-frontend',
-    script: '/usr/bin/serve',
-    args: 'build -s -p 3000',
-    instances: 1,
-    exec_mode: 'fork',
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '500M',
-    env_production: {
-      NODE_ENV: 'production',
-      PM2_SERVE_SPA: 'true',
-      PM2_SERVE_HOMEPAGE: '/index.html'
-    },
-    error_file: 'logs/frontend-err.log',
-    out_file: 'logs/frontend-out.log',
-    log_file: 'logs/frontend-combined.log',
-    time: true,
-    merge_logs: true,
-    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-  }]
-}; 
+    apps: [{
+      name: 'planejador-frontend',
+      script: 'node',
+      args: '/usr/bin/serve build -s -p 3000',
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env_production: {
+        NODE_ENV: 'production',
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html'
+      },
+      error_file: 'logs/frontend-err.log',
+      out_file: 'logs/frontend-out.log',
+      log_file: 'logs/frontend-combined.log',
+      time: true,
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    }]
+  };
+  
