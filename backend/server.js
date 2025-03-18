043623082b2eca436806d41276b44ee61ea14d98
Luiz Gustavo <luizguestape54@hotmail.com>
2025-03-18 18:59:05 -0300

    vamo!

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
