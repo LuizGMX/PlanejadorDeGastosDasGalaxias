b6307ab6b5dd6e7098019d88e40313a50e544606
Luiz Gustavo <luizguestape54@hotmail.com>
2025-04-04 10:45:34 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,7 @@ app.get('*', (req, res) => {
 // Definir o servidor
 let server;
 
+
 if (process.env.NODE_ENV === 'production') {
   // Carregar certificados apenas se for ambiente de produção
   const privateKey = fs.readFileSync('/etc/letsencrypt/live/planejadordasgalaxias.com.br/privkey.pem', 'utf8');
