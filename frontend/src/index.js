8412de814b0f139477cbe8497576d9b38b8650ed
Luiz Gustavo <luizguestape54@hotmail.com>
2025-04-06 16:21:41 -0300

    IOS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Importa createRoot
 import './index.css';
+import './styles/ios.css';
 import App from './App';
+import initIOSFixes from './utils/ios-fixes';
+
+// Initialize iOS fixes
+initIOSFixes();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
